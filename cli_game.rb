d37109edd3ea405c3160ff2ca7aaadcb3e2726af
Robby Ranshous <rranshous@gmail.com>
2020-03-31 15:48:45 -0400

    default inputs

--- cli_game.rb
+++ cli_game.rb
@@ -1,8 +1,9 @@
 require_relative 'sim'
 
-def ask_i question
-  puts question
-  gets.to_i
+def ask_i question, default: 0
+  puts question + "[#{default}]"
+  answer = gets.chomp
+  answer.empty? ? default : answer.to_i
 end
 
 class CliGame
@@ -13,10 +14,13 @@ class CliGame
   end
 
   def set_inputs
-    sim.set_miner_count ask_i('How many miners should we have?')
-    sim.set_processor_count ask_i('How many processors should we have?')
-    sim.set_seller_count ask_i('How many sellers should we have?')
-    self.outstanding_cycles = ask_i('How many cycles should we run?')
+    sim.set_miner_count ask_i('How many miners should we have?',
+                              default: sim.get_miner_count)
+    sim.set_processor_count ask_i('How many processors should we have?',
+                                  default: sim.get_processor_count)
+    sim.set_seller_count ask_i('How many sellers should we have?',
+                               default: sim.get_seller_count)
+    self.outstanding_cycles = ask_i('How many cycles should we run?', default: 1)
   end
 
   def run_work_cycle
@@ -31,7 +35,7 @@ class CliGame
     puts "remaining to be mined: #{sim.get_mine_product}"
     puts "mined resources[#{sim.get_miner_count}]: #{sim.get_miner_product}"
     puts "processed resouces[#{sim.get_processor_count}]: #{sim.get_processor_product}"
-    puts "sold resources[#{sim.get_seller_count}]: #{sim.get_seller_product}"
+    puts "sellers[#{sim.get_seller_count}]"
     puts "credits earned: #{sim.get_earned}"
     puts '-' * 50
   end
